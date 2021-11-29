d9d48e12cb611805204f3381988ef7ecb862f4a6
Tre Bourdon <calmbat360@gmail.com>
2021-11-29 17:36:28 -0500

    Basic GUI added

--- Client/ChatClient.py
+++ Client/ChatClient.py
@@ -1,14 +1,13 @@
 import socket
 import threading
+import PySimpleGUI as sg
 
+#client code
 class ChatClient:
     def __init__(self, HOST: str = socket.gethostname(), PORT: int = 5555):
         self.address = (HOST, PORT)
-        print("first test")
-        self.connectToServer()
-        print("second test")
-        self.createRecievingThread()
-        print("third test")
+        #self.connectToServer()
+        #self.createRecievingThread()
         self.chat()
 
     def connectToServer(self):
@@ -41,12 +40,40 @@ class ChatClient:
 
     # used for client to be sending chat messages
     def chat(self):
+        window = homeInit()
         while True:
-            print("Choose to send rock, paper, or scissors\n")
-            print("(type r, p, or s and then enter)\n")
-            message = input()
-            self.sendMessage(message)
-            if message == "{quit}": break
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Quit': # if user closes window or clicks cancel
+                break
+            if event == 'Enter':
+                print('You entered ', values[0])
+                #self.sendMessage(values[0])
+            if event == 'Next Page':
+                window = gameInit(window)
+                
+            
+
+#gui code
+def homeInit():
+    sg.theme('DarkAmber')
+    layout = [  [sg.Text('Welcome to RPS')],
+                [sg.Text('Enter your Username: '), sg.InputText()],
+                [sg.Button('Enter'), sg.Button('Quit'), sg.Button('Next Page')] ]
+
+    # Create the Window
+    window = sg.Window('RPS Game', layout)
+    return window
+
+def gameInit(window):
+    sg.theme('DarkAmber')
+    layout = [  [sg.Text('Welcome to the Game Screen')],
+                [sg.Button('Rock'), sg.Button('Paper'), sg.Button('Scissors')] ]
+
+    # Create the Window
+    window.close()
+    window = sg.Window('RPS Game', layout)
+    return window
+
 
 if __name__ == '__main__':
     ChatClient()
